25d7fc99baa6a43cb3d0e3b3c202949f31768d4d
Guizzy89 <guizzyilgizgareev@gmail.com>
2025-08-26 16:14:39 +0300

    Обновил конфигурацию подключения в файле Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,12 @@
+using AMassage.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
