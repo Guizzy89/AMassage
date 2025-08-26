6ebbc61befdc7dc823b14cebb876ea3f070db3ac
Guizzy89 <guizzyilgizgareev@gmail.com>
2025-08-26 16:12:39 +0300

    Создал модели для сущностей Massage и Booking

---
+++ Models/Booking.cs
@@ -0,0 +1,9 @@
+﻿public class Booking
+{
+    public int Id { get; set; }
+    public DateTime SlotDate { get; set; }
+    public bool IsAvailable { get; set; }
+    public string ClientName { get; set; }
+    public string PhoneNumber { get; set; }
+    public string Comment { get; set; }
+}
