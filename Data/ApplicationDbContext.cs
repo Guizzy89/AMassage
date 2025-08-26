56c721062f07a3119e176f1ea3b583f699c84b90
Guizzy89 <guizzyilgizgareev@gmail.com>
2025-08-26 16:13:53 +0300

    Создал контекст базы данных в папке Data Настроил подключение к SQL Server в файле appsettings.json

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AMassage.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Massage> Massages { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+    }
+}
